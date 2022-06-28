deb982b3cba93ef2f30d4455d43cff45f2f68d56
Allen McPherson <al@losalamosal.me>
2022-06-28 21:45:52 +0000

    Doing a litte reorganizing

---
+++ apps/book-search/offline/lambda/rekog.js
@@ -0,0 +1,26 @@
+const aws = require('aws-sdk');
+const rek = new aws.Rekognition();
+
+exports.handler = async (event, context) => {
+  const bucket = event.Records[0].s3.bucket.name;
+  const photo = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '));
+  const params = {
+    Image: {
+      S3Object: {
+        Bucket: bucket,
+        Name: photo
+      }
+    }
+  }
+
+  console.log(`${bucket}   ${photo}`)
+
+  let response = await rek.detectText(params).promise()
+  console.log(`Detected Text for: ${photo}`)
+  
+  response.TextDetections.forEach(label => {
+    console.log(`Detected Text: ${label.DetectedText}`)
+    console.log(`Type: ${label.Type}`)
+    console.log(`ID: ${label.Id}`)
+  })
+}
