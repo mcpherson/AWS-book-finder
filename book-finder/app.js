209b0653fc7946d53704aef41398012336f8b4e3
Marlon McPherson <583056+mcpherson@users.noreply.github.com>
2022-07-19 06:41:28 +0000

    Log out logic will now only load if a log out link exists.

--- book-finder/app.js
+++ book-finder/app.js
@@ -16,28 +16,30 @@ window.onload = () => {
 
 
 // LOG OUT
-logoutNav.addEventListener('click', () => {
-    const logoutData = {
-        accessToken : JSON.parse(localStorage.getItem('book-finder-login-data')).AuthenticationResult.AccessToken
-    }
-    const logoutReq = new XMLHttpRequest();
-    logoutReq.open("POST", "https://4y5tf8v53d.execute-api.us-west-2.amazonaws.com/dev/logout");
-    logoutReq.send(JSON.stringify(logoutData));
-
-    logoutReq.onload = function() {
-        if (logoutReq.status != 200) { // analyze HTTP status of the response
-            console.log(`Error ${logoutReq.status}: ${logoutReq.statusText}`);
-            // alertArea.style.display = "block";
-            // alertArea.style.backgroundColor = "lightcoral";
-            // alertMessage.innerText = "Invalid logout information. Please check your email address and re-type your password and try again. Make sure that your account has been verified. Reset password if necessary.";
-            throw new Error("Logout failed. See console for details.");
-        } else {
-            // console.log(logoutReq.response); // response is the server response
-            localStorage.removeItem('book-finder-login-data');
-            window.location.href = "/book-finder/index.html";
+if (logoutNav != null) {
+    logoutNav.addEventListener('click', () => {
+        const logoutData = {
+            accessToken : JSON.parse(localStorage.getItem('book-finder-login-data')).AuthenticationResult.AccessToken
         }
-    };
-});
+        const logoutReq = new XMLHttpRequest();
+        logoutReq.open("POST", "https://4y5tf8v53d.execute-api.us-west-2.amazonaws.com/dev/logout");
+        logoutReq.send(JSON.stringify(logoutData));
+    
+        logoutReq.onload = function() {
+            if (logoutReq.status != 200) { // analyze HTTP status of the response
+                console.log(`Error ${logoutReq.status}: ${logoutReq.statusText}`);
+                // alertArea.style.display = "block";
+                // alertArea.style.backgroundColor = "lightcoral";
+                // alertMessage.innerText = "Invalid logout information. Please check your email address and re-type your password and try again. Make sure that your account has been verified. Reset password if necessary.";
+                throw new Error("Logout failed. See console for details.");
+            } else {
+                // console.log(logoutReq.response); // response is the server response
+                localStorage.removeItem('book-finder-login-data');
+                window.location.href = "/book-finder/index.html";
+            }
+        };
+    });
+}
 
 
     
