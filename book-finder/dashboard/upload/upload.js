22cecd331a16ab12d054056ca05281b1f3e537e9
Marlon McPherson <583056+mcpherson@users.noreply.github.com>
2022-08-04 23:20:40 +0000

    More auth testing

--- book-finder/dashboard/upload/upload.js
+++ book-finder/dashboard/upload/upload.js
@@ -302,6 +302,7 @@ uploadButton.addEventListener('click', () => {
     uploadReq.open("POST", "https://4y5tf8v53d.execute-api.us-west-2.amazonaws.com/dev/S3-upload");
     uploadReq.setRequestHeader('Authorization', 'Bearer ' + JSON.parse(localStorage.getItem('book-finder-login-data')).AuthenticationResult.AccessToken);
     uploadReq.setRequestHeader('Accept', 'application/json');
+    console.log(uploadReq);
 
     uploadReq.send(JSON.stringify(uploadData));
 
