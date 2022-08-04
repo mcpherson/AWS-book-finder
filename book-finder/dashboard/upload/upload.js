af5b96d5e74bda8ed63a84d66688ab06ee1f39a3
Marlon McPherson <583056+mcpherson@users.noreply.github.com>
2022-08-04 22:22:11 +0000

    testing api authorization...

--- book-finder/dashboard/upload/upload.js
+++ book-finder/dashboard/upload/upload.js
@@ -300,7 +300,8 @@ uploadButton.addEventListener('click', () => {
     const uploadReq = new XMLHttpRequest();
 
     uploadReq.open("POST", "https://4y5tf8v53d.execute-api.us-west-2.amazonaws.com/dev/S3-upload");
-    uploadReq.setRequestHeader('Authentication', JSON.parse(localStorage.getItem('book-finder-login-data')).AuthenticationResult.IdToken);
+    uploadReq.setRequestHeader('Authorization', JSON.parse(localStorage.getItem('book-finder-login-data')).AuthenticationResult.IdToken);
+
     uploadReq.send(JSON.stringify(uploadData));
 
     uploadReq.onload = function() {
