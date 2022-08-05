4cfeb436846a48a1ec74980be5a5ba4484b97269
Marlon McPherson <583056+mcpherson@users.noreply.github.com>
2022-08-05 21:21:53 +0000

    Added option to manually refresh images in library. This will reset localstorage URL/count tracking. Also added logic to update localstorage variables during library S3 fetch.

--- book-finder/dashboard/library/library.js
+++ book-finder/dashboard/library/library.js
@@ -19,50 +19,53 @@ window.onload = () => {
     
     // DISPLAY EACH IMAGE AND KEY
     JSON.parse(localStorage.getItem('imageURLs')).forEach((i) => {
-        console.log(i);
+
     }); 
 }
 
 const getImageURLs = function(event) {
     // CLEAR AND RESET LOCALSTORAGE FOR MANUAL REFRESH
     if (event) {
-        console.log(event);
-    } else {
-        const reqData = {UserSub: JSON.parse(localStorage.getItem('book-finder-login-data')).UserSub}
-            
-        const keysReq = new XMLHttpRequest();
-        
-        keysReq.open("POST", "https://4y5tf8v53d.execute-api.us-west-2.amazonaws.com/dev/get-s3-keys");
-        keysReq.setRequestHeader('Authorization', 'Bearer ' + JSON.parse(localStorage.getItem('book-finder-login-data')).AuthenticationResult.IdToken);
-        keysReq.send(JSON.stringify(reqData));
-        
-        keysReq.onload = function() {
-            if (JSON.parse(keysReq.response).length === 0) { // no keys returned
-                loadingSpinner.style.display = "none";
-                alertArea.style.display = "block";
-                alertArea.style.backgroundColor = "lightcoral";
-                alertMessage.innerHTML = 'No images found. <a href="/book-finder/dashboard/upload/">Upload some images</a> and they will appear here.';
-            } else if (keysReq.status != 200 || JSON.parse(keysReq.response).hasOwnProperty('errorType')) { // error received
-                // TODO error handling - invalid usersub, invalid img data, other AWS errors
-                loadingSpinner.style.display = "none";
-                throw new Error("Image request failed.");
-            } else { // create and store URLs
-                loadingSpinner.style.display = "none";
-                let objKeys = JSON.parse(keysReq.response);
-                console.log(objKeys);
-                objKeys.forEach((i) => {
-                    let urlObj = {
-                        "Key" : JSON.parse(keysReq.response)[i],
-                        "imageURL" : `https://book-finder-${reqData.UserSub}.s3.amazonaws.com/${JSON.parse(keysReq.response)[i]}`
-                    };
-                    let storedURLs = JSON.parse(localStorage.getItem('imageURLs'));
-                    storedURLs.push(urlObj);
-                    localStorage.setItem('imageURLs', JSON.stringify(storedURLs));
-                });
-            }
-        };
+        localStorage.removeItem('numUploads');
+        localStorage.removeItem('imageURLs');
+        localStorage.setItem('numUploads', JSON.stringify(0));
+        let imageURLs = [];
+        localStorage.setItem('imageURLs', JSON.stringify(imageURLs));
     }
-}
+    // GET IMAGE KEYS FROM USER'S S3 BUCKET
+    const reqData = {UserSub: JSON.parse(localStorage.getItem('book-finder-login-data')).UserSub}
+    const keysReq = new XMLHttpRequest();
+    keysReq.open("POST", "https://4y5tf8v53d.execute-api.us-west-2.amazonaws.com/dev/get-s3-keys");
+    keysReq.setRequestHeader('Authorization', 'Bearer ' + JSON.parse(localStorage.getItem('book-finder-login-data')).AuthenticationResult.IdToken);
+    keysReq.send(JSON.stringify(reqData));
+    keysReq.onload = function() {
+        if (JSON.parse(keysReq.response).length === 0) { // no keys returned
+            loadingSpinner.style.display = "none";
+            alertArea.style.display = "block";
+            alertArea.style.backgroundColor = "lightcoral";
+            alertMessage.innerHTML = 'No images found. <a href="/book-finder/dashboard/upload/">Upload some images</a> and they will appear here.';
+        } else if (keysReq.status != 200 || JSON.parse(keysReq.response).hasOwnProperty('errorType')) { // error received
+            // TODO error handling - invalid usersub, invalid img data, other AWS errors
+            loadingSpinner.style.display = "none";
+            throw new Error("Image request failed.");
+        } else { // create and store URLs
+            loadingSpinner.style.display = "none";
+            let objKeys = JSON.parse(keysReq.response);
+            objKeys.forEach((i) => {
+                let urlObj = {
+                    "Key" : JSON.parse(keysReq.response)[i],
+                    "imageURL" : `https://book-finder-${reqData.UserSub}.s3.amazonaws.com/${JSON.parse(keysReq.response)[i]}`
+                };
+                let storedURLs = JSON.parse(localStorage.getItem('imageURLs'));
+                storedURLs.push(urlObj);
+                localStorage.setItem('imageURLs', JSON.stringify(storedURLs));
+                let numUploads = JSON.parse(localStorage.getItem('numUploads'));
+                numUploads++;
+                localStorage.setItem('numUploads', JSON.stringify(numUploads));
+            });
+        };
+    };
+};
 
 // REFRESH IMAGES MANUALLY
 refreshButton.addEventListener('click', getImageURLs);
