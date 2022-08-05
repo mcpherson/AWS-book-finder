61c4be5b0d08619c32f2dff3562ba5de1c3d98f1
Marlon McPherson <583056+mcpherson@users.noreply.github.com>
2022-08-05 21:35:50 +0000

    Added logging for testing...

--- book-finder/dashboard/library/library.js
+++ book-finder/dashboard/library/library.js
@@ -51,6 +51,7 @@ const getImageURLs = function(event) {
             let objKeys = JSON.parse(keysReq.response);
             localStorage.setItem('numUploads', JSON.stringify(objKeys.length)); // store number of uploaded images for comparison on pageload
             objKeys.forEach((i) => {
+                console.log(JSON.parse(keysReq.response));
                 let urlObj = {
                     "Key" : JSON.parse(keysReq.response)[i],
                     "imageURL" : `https://book-finder-${reqData.UserSub}.s3.amazonaws.com/${JSON.parse(keysReq.response)[i]}`
