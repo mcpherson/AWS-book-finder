78189fd9bc513d4ce06d543b03e1999411cbb7a6
Marlon McPherson <583056+mcpherson@users.noreply.github.com>
2022-08-08 01:24:25 +0000

    More delete testing...

--- book-finder/dashboard/library/library.js
+++ book-finder/dashboard/library/library.js
@@ -145,7 +145,7 @@ const deleteImage = function(selectedImageNumber) {
         } else {
             // UPDATE LOCALSTORAGE
             localStorage.setItem('numUploads', JSON.parse(localStorage.getItem('numUploads'))-1);
-            localStorage.setItem('imageURLs', JSON.parse(localStorage.getItem('imageURLs')).splice(selectedIndex, 1));
+            localStorage.setItem('imageURLs', JSON.stringify(JSON.parse(localStorage.getItem('imageURLs')).splice(selectedIndex, 1)));
             // UPDATE UI
             document.getElementById(`library-item-${selectedImageNumber}`).remove();
         }
