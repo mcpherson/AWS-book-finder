0a566ecbcf2d55f7804888bce2c65e5a8bb63874
Marlon McPherson <583056+mcpherson@users.noreply.github.com>
2022-11-05 22:51:01 +0000

    Updated global config file - each API endpoint is now parameterized and referenceable from config.js.

---
+++ book-finder/config.js
@@ -0,0 +1,13 @@
+const apiEndpointID = "ard3w89yz0";
+const apiStage = "dev";
+const awsRegion = "us-east-1";
+const apiEndpoints = {
+    API_USER_SIGNUP             : `https://${apiEndpointID}.execute-api.${awsRegion}.amazonaws.com/${apiStage}/user/signup`,
+    API_USER_CONFIRM_SIGNUP     : `https://${apiEndpointID}.execute-api.${awsRegion}.amazonaws.com/${apiStage}/user/confirm-signup`,
+    API_USER_LOGIN              : `https://${apiEndpointID}.execute-api.${awsRegion}.amazonaws.com/${apiStage}/user/login`,
+    API_USER_LOGOUT             : `https://${apiEndpointID}.execute-api.${awsRegion}.amazonaws.com/${apiStage}/user/logout`,
+    API_USER_DELETE             : `https://${apiEndpointID}.execute-api.${awsRegion}.amazonaws.com/${apiStage}/user/delete`,
+    API_LIBRARY                 : `https://${apiEndpointID}.execute-api.${awsRegion}.amazonaws.com/${apiStage}/library`,
+    API_LIBRARY_UPLOAD          : `https://${apiEndpointID}.execute-api.${awsRegion}.amazonaws.com/${apiStage}/library/upload`,
+    API_LIBRARY_SEARCH          : `https://${apiEndpointID}.execute-api.${awsRegion}.amazonaws.com/${apiStage}/library/search`
+};
--- book-finder/globals.js
+++
@@ -1,1 +0,0 @@
-const apiEndpointID = "ard3w89yz0";
