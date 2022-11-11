55f54ba0f933b45605fa22deb85b34441af46cf1
Marlon McPherson <583056+mcpherson@users.noreply.github.com>
2022-11-11 17:09:12 +0000

    Fixed HTML resource paths (again). Also testing fetch to replace xhttp requests.

--- book-finder/login/login.js
+++ book-finder/login/login.js
@@ -21,25 +21,54 @@ loginButton.addEventListener('click', (event) => {
         password: passwordField.value
     };
 
-    const loginReq = new XMLHttpRequest();
-    loginReq.open("POST", `https://${apiEndpointID}.execute-api.us-east-1.amazonaws.com/dev/user/login`);
-    loginReq.send(JSON.stringify(loginData));
+    // const loginReq = new XMLHttpRequest();
+    // loginReq.open("POST", `https://${apiEndpointID}.execute-api.us-east-1.amazonaws.com/dev/user/login`);
+    // loginReq.send(JSON.stringify(loginData));
 
-    loginReq.onload = function() {
-        if (loginReq.status != 200 || JSON.parse(loginReq.response).hasOwnProperty('__type')) { // analyze HTTP status of the response
-            formArea.style.visibility = "visible";
-            spinner.style.display = "none";
-            console.log(`Error ${loginReq.status}: ${loginReq.statusText} - AWS Error: ${loginReq.response}`);
-            alertArea.style.display = "block";
-            alertArea.style.backgroundColor = "lightcoral";
-            alertMessage.innerText = "Invalid login information. Please check your email address and re-type your password and try again. Make sure that your account has been verified. Reset password if necessary.";
-            throw new Error("Login failed. Enter correct information or see console for details. Reset password if necessary.");
-        } else {
-            console.log(loginReq.response); // response is the server response
-            localStorage.setItem('book-finder-login-data', loginReq.response);
-            window.location.href = "../library/";
-        }
-    };
+    cognitoLogin(apiEndpoints.API_USER_LOGIN, loginData)
+    .then((data) => {
+        console.log(data);
 
+    });
 
+    // loginReq.onload = function() {
+    //     if (loginReq.status != 200 || JSON.parse(loginReq.response).hasOwnProperty('__type')) { // analyze HTTP status of the response
+    //         formArea.style.visibility = "visible";
+    //         spinner.style.display = "none";
+    //         console.log(`Error ${loginReq.status}: ${loginReq.statusText} - AWS Error: ${loginReq.response}`);
+    //         alertArea.style.display = "block";
+    //         alertArea.style.backgroundColor = "lightcoral";
+    //         alertMessage.innerText = "Invalid login information. Please check your email address and re-type your password and try again. Make sure that your account has been verified. Reset password if necessary.";
+    //         throw new Error("Login failed. Enter correct information or see console for details. Reset password if necessary.");
+    //     } else {
+    //         console.log(loginReq.response); // response is the server response
+    //         localStorage.setItem('book-finder-login-data', loginReq.response);
+    //         window.location.href = "../library/";
+    //     }
+    // };
 });
+
+// fetch (POST) for Cognito login
+async function cognitoLogin(url = '', data = {}) {
+
+    const response = await fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'omit',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify(data)
+    });
+
+    return response.json();
+};
+
+// call fetch (POST) for Cognito login
+cognitoLogin(apiEndpoints.API_USER_LOGIN, loginData)
+    .then((data) => {
+        console.log(data);
+    });
