0a9eaafd347fcbdcc97c3459e08d54488cbce5c7
mcpherson <583056+mcpherson@users.noreply.github.com>
2023-04-09 20:16:54 +0000

    homepage UI improvements

--- book-finder/home.js
+++ book-finder/home.js
@@ -1,5 +1,19 @@
+// adjust vertical alignment of home content
+const setMarginTop = function() {
+
+    let homeContainer = document.getElementById('container')
+    let homeHeight = homeContainer.offsetHeight
+    homeContainer.style.marginTop = `${(window.innerHeight - homeHeight) / 2.7}px`
+
+}
+
+window.addEventListener('resize', setMarginTop)
+
 window.onload = () => {
     
     setUserState() // in global js file
     Array.from(document.getElementsByTagName('html'))[0].style.visibility = 'visible'
+
+    setMarginTop()
+    
 }
